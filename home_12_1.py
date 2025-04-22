dd0b055c4e0fe0ec8f02c13483928c8851322d12
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2025-04-22 18:55:20 +0300

    12_1

---
+++ home_12_1.py
@@ -0,0 +1,21 @@
+import codecs
+import re
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        html = file.read()
+
+    clean_text = re.sub(r'<[^>]+>', '', html)
+
+    clean_text = re.sub(r'&[a-zA-Z0-9#]+;', '', clean_text)
+
+    cleaned_lines = []
+    for line in clean_text.splitlines():
+        stripped = line.strip()
+        if stripped:  # Якщо рядок не порожній після видалення пробілів
+            cleaned_lines.append(stripped)
+
+    with codecs.open(result_file, 'w', 'utf-8') as output:
+        output.write('\n'.join(cleaned_lines))
+
+delete_html_tags('draft.html')
